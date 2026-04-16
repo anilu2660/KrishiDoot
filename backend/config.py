66ca0fee029a8a962366e99bc2bb7fe91346b512
Anuj Anil Upadhyay <upadhyayanuj526@gmail.com>
2026-04-16 23:22:08 +0530

    Update DATA_GOV_API_KEY with a new value

--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATA_GOV_API_KEY: str = "DEMO_KEY"
+    DATA_GOV_API_KEY: str = "579b464db66ec23bdd000001d48b1f69dc574a3745bb3f1fc4046969"
     GEMINI_API_KEY: str = ""
     SUPABASE_URL: str = ""
     SUPABASE_KEY: str = ""
